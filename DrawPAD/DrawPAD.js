2696476db0f808893ac792988a33d570449f7f62
ageel911 <ageel911@gmail.com>
2017-02-14 00:18:00 +0300

    Update DrawPAD.js

--- DrawPAD/DrawPAD.js
+++ DrawPAD/DrawPAD.js
@@ -6,7 +6,7 @@ function DrawPad() {
     this.initiate = function() {
 
         for (i = 0; i < 500; i=i+.1) {
-            this.addElement(new Line(i*20,cos(i)*300+100, i*20*(1+cos(i)), cos(i+.1)*500+100*+cos(i), 1, 255));
+            this.addElement(new Line(i*20,500-i,255));
 
         }
     }
