c2465d320bb9ce8574befa80c11db50a01ccda0c
Ageel <ageel911@gmail.com>
2017-02-14 06:49:42 +0300

    Adding Copyright Notice to files

--- DrawPAD/DrawPAD.js
+++ DrawPAD/DrawPAD.js
@@ -1,3 +1,18 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 function DrawPad() {
 
     var currentTool;
@@ -5,9 +20,8 @@ function DrawPad() {
 
     this.initiate = function() {
 
-        for (i = 0; i < 500; i=i+.1) {
-            this.addElement(new Line(i*20,i*10,500- i,i*11,3,255));
-                                                                                                                                                                                                                                                                                                                                                                                               
+        for (i = 0; i < 500; i = i + .1) {
+            this.addElement(new Line(i * 20, i * 10, 500 - i, i * 11, 3, 255));
         }
     }
 
@@ -25,5 +39,4 @@ function DrawPad() {
     this.addElement = function(x) {
         elements.push(x);
     }
-
 }
